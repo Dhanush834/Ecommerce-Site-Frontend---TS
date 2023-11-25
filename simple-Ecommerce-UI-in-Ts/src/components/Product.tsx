03f5dbef1366b7f614934ab661e3cc3ed0a03ba3
Dhanush U <124449985+Dhanush834@users.noreply.github.com>
2023-11-26 00:29:28 +0530

    Products Page Done 🎉

--- simple-Ecommerce-UI-in-Ts/src/components/Product.tsx
+++ simple-Ecommerce-UI-in-Ts/src/components/Product.tsx
@@ -1,19 +1,39 @@
 import React, { ReactElement } from 'react'
 import { ProductsType } from '../context/ProductsProvider'
-import { ReducerActionType , ReducerAction } from '../context/CartProvider'
+import { ReducerActionType, ReducerAction } from '../context/CartProvider'
 
 type PropsType = {
-    product : ProductsType
-    dispatch : React.Dispatch<ReducerAction>
-    REDUCER_ACTION : ReducerActionType
-    isCart : boolean
+  product: ProductsType
+  dispatch: React.Dispatch<ReducerAction>
+  REDUCER_ACTION: ReducerActionType
+  isCart: boolean
 }
 
-const Product = ({product,dispatch,REDUCER_ACTION,isCart}:PropsType) : ReactElement => {
+const Product = ({ product, dispatch, REDUCER_ACTION, isCart }: PropsType): ReactElement => {
+
+  const img: string = new URL(`../imgs/${product.sku}.jpg`, import.meta.url).href
+  // import.meta.url -> gives the path of current module which is the base url
+
+  //-------------------- Functions dealing with Cart -----------
+
+  const onAddToCart = () => {
+    dispatch({
+      type: REDUCER_ACTION.ADD,
+      payload: { ...product, quantity: 1 }
+    })
+  }
+
+  const itemInCart = isCart ? ' → Item in Cart: ✅' : null
+
   return (
-    <div>
-      
-    </div>
+    <article className="product">
+      <h3>{product.name}</h3>
+      <img src={img} alt={product.name} className="product__img" />
+      <p>
+        {new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(product.price)}{itemInCart}
+      </p>
+      <button onClick={onAddToCart}>Add to Cart</button>
+    </article>
   )
 }
 
--- simple-Ecommerce-UI-in-Ts/src/context/ProductsProvider.tsx
+++ simple-Ecommerce-UI-in-Ts/src/context/ProductsProvider.tsx
@@ -19,7 +19,25 @@ type ChildrenType = {
 
 //--------------- Default Values ---------
 
-const initProductState : ProductsType[] = []  //Empty array for useState before api call or default value
+// const initProductState : ProductsType[] = []  //Empty array for useState before api call or default value
+
+const initProductState : ProductsType[] = [
+    {
+        "sku": "item01",
+        "name": "Widget",
+        "price": 9.99
+    },
+    {
+        "sku": "item02",
+        "name": "Premium Widget",
+        "price": 19.99
+    },
+    {
+        "sku": "item03",
+        "name": "Deluxe Widget",
+        "price": 29.99
+    }
+]
 
 const initContextState :ProductsContextType = { products:[] } // Zero products in cart before calling to api
 
@@ -30,28 +48,31 @@ export const ProductsContext = createContext<ProductsContextType>(initContextSta
 
 export const ProductsProvider = ({children}:ChildrenType) : ReactElement => {
 
-    useEffect(()=>{
-        const fetchProduts = async () : Promise<ProductsType[]> => {
 
-            const data = await fetch('http://localhost:4000/products')
-            .then(res=>{
-                return res.json();
-            })
-            .catch(err=>{
-                if(err instanceof Error){
-                    console.log(err);
-                }
-            })
-            console.log(data);
+    // use this when u start a server for json app
+
+    // useEffect(()=>{
+    //     const fetchProduts = async () : Promise<ProductsType[]> => {
+
+    //         const data = await fetch('http://localhost:4000/products')
+    //         .then(res=>{
+    //             return res.json();
+    //         })
+    //         .catch(err=>{
+    //             if(err instanceof Error){
+    //                 console.log(err);
+    //             }
+    //         })
+    //         console.log(data);
             
-            return data;
-        }
+    //         return data;
+    //     }
 
-        fetchProduts()
-        .then(res=>setProducts(res))  //Products loaded after calling api
-        .catch(err=>console.log(err));
+    //     fetchProduts()
+    //     .then(res=>setProducts(res))  //Products loaded after calling api
+    //     .catch(err=>console.log(err));
 
-    },[])
+    // },[])
 
     const [products,setProducts] = useState(initProductState)
 
