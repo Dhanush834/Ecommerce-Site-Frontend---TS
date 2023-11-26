5d5666acb82575cb02bed4ed487dce5110e06cfa
Dhanush U <124449985+Dhanush834@users.noreply.github.com>
2023-11-26 13:02:58 +0530

    Cart Functionalities Done

--- simple-Ecommerce-UI-in-Ts/src/components/Cart.tsx
+++ simple-Ecommerce-UI-in-Ts/src/components/Cart.tsx
@@ -1,10 +1,60 @@
-import React from 'react'
+import React, { useState } from 'react'
+import useCart from '../hooks/useCart'
+import CartLine from './CartLine'
+
 
 const Cart = () => {
+
+  const [confirm,setConfirm] = useState(false);
+
+  const {
+    dispatch,
+    REDUCER_ACTION,
+    totalItems,
+    totalPrice,
+    cart
+  } = useCart()
+
+  const onSubmitOrder = () => {
+    dispatch({
+      type:REDUCER_ACTION.SUBMIT
+    })
+    setConfirm(true)
+  }
+
+  const content = 
+  <>
+    { confirm 
+    ? <h2>Thank You for your Order</h2> :
+      <>
+        <h2 className="offscreen">Cart</h2>
+        <ul className="cart">
+                {cart.map(item => {
+                    return (
+                        <CartLine
+                            key={item.sku}
+                            item={item}
+                            dispatch={dispatch}
+                            REDUCER_ACTION={REDUCER_ACTION}
+                        />
+                    )
+                })}
+          </ul>
+          <div className="cart__totals">
+                <p>Total Items: {totalItems}</p>
+                <p>Total Price: {totalPrice}</p>
+                <button className="cart__submit" disabled={!totalItems} onClick={onSubmitOrder}>
+                    Place Order
+                </button>
+          </div>
+      </>
+    }
+  </>
+
   return (
-    <div>
-      
-    </div>
+    <main className="main main--cart">
+            {content}
+    </main>
   )
 }
 
